517c73cb33a54c4a35cd7fa1c9ead8b2c3cbaf76
Leena <109791804+leenahunagund@users.noreply.github.com>
2024-02-26 22:51:40 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class arrays {
+    public static void main(String[] args) {
+        int[] a={1,6,3,7,2,78,331,4};//int[] n == int n[]
+        Arrays.sort(a);
+        for(int ele:a)
+        System.out.print(ele+" ");
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(a));
+    }
+}
